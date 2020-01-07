c8556aea258d8bc01c4e16ecd390d02914338cdb
Luke Storry <luke@lukestorry.co.uk>
2020-01-07 21:28:18 +0000

    Revert "wrapped data file in with "data" object for easier cms"
    This reverts commit a032b6cfee6e20d28a6e9189b246bea65afb7651.

--- assets/accordion/create-accordion.js
+++ assets/accordion/create-accordion.js
@@ -6,7 +6,7 @@ fetch('/assets/authenticlearningdata.json')
   .then(addClickListeners);
 
 function createAccordion(file) {
-  const data = JSON.parse(file).data;
+  const data = JSON.parse(file);
   console.log(data);
   let root = document.getElementById('accordion');
   data.forEach(dataSection => appendSection(dataSection, root));
