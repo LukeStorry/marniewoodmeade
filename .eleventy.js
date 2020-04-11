c22e2fc3038135aea25d886c10d69d7299790eca
Luke Storry <luke@lukestorry.co.uk>
2020-04-11 19:56:58 +0100

    prettier config

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,22 @@
 var nestedAccordion = require("nested-accordion");
 var authenticLearningDataFile = require("./assets/authenticlearningdata.json");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addCollection("navbar_links", collection => {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addCollection("navbar_links", (collection) => {
     return collection
       .getAll()
-      .filter(page => page.data.in_navbar)
+      .filter((page) => page.data.in_navbar)
       .sort((a, b) => a.data.in_navbar - b.data.in_navbar);
   });
 
-  eleventyConfig.addCollection("blog", collection =>
+  eleventyConfig.addCollection("blog", (collection) =>
     collection
       .getFilteredByGlob("*/blog/*.md")
       .sort((a, b) => (b.data.date > a.data.date ? 1 : -1))
       .sort((a, b) => (b.data.display_order || 0) - (a.data.display_order || 0))
   );
 
-  eleventyConfig.addCollection("articles", collection =>
+  eleventyConfig.addCollection("articles", (collection) =>
     collection
       .getFilteredByGlob("*/articles/*.md")
       .sort((a, b) => (b.data.date > a.data.date ? 1 : -1))
@@ -24,8 +24,8 @@ module.exports = function(eleventyConfig) {
   );
 
   eleventyConfig.addShortcode("authentic_learning_accordion", () => {
-    var data = JSON.parse(authenticLearningDataFile.data)
-    data.forEach(location => delete location.text); // remove gps coordinates
+    var data = JSON.parse(authenticLearningDataFile.data);
+    data.forEach((location) => delete location.text); // remove gps coordinates
     return nestedAccordion.create(data);
   });
 
@@ -45,9 +45,9 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "src",
       includes: "_layouts",
-      output: "./_site"
+      output: "./_site",
     },
 
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
 };
