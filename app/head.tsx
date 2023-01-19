8064a0f79b6993ae83b7ca57d9c883ecec7112c7
Luke Storry <luke.storry@letsdothis.com>
2023-01-19 10:50:49 +0000

    basic seo

--- app/head.tsx
+++ app/head.tsx
@@ -1,15 +1,28 @@
 export default function Head() {
   return (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <meta
-        name="description"
-        content="Welcome to my homepage, blog & portfolio."
-      />
-      <title>Marnie Woodmeade</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <link rel="icon" href="/favicon.ico" />
+      <title>Marnie Woodmeade</title>
+
+      <meta
+        name="description"
+        content="Welcome to my homepage, blog &amp; portfolio."
+      />
+      <meta
+        property="og:description"
+        content="Welcome to my homepage, blog &amp; portfolio."
+      />
+
+      <meta name="twitter:creator" content="@marnie_wm" />
+      <meta
+        name="twitter:card"
+        content="Welcome to my homepage, blog &amp; portfolio."
+      />
+      <meta property="og:site_name" content="Marnie Woodmeade" />
+      <meta property="og:title" content="Marnie Woodmeade" />
     </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <head />
-      <body className="mx-auto max-w-md bg-gray-200">
+      <body className="mx-auto mt-6 max-w-md bg-gray-200">
         <header className="mx-auto max-w-md">
           <h1 className="text-center text-xl">Marnie Woodemeade</h1>
         </header>
