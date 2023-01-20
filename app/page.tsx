b90dfa3eae52dbeb492664f6ff1e7b733688abd3
Luke Storry <luke.storry@letsdothis.com>
2023-01-20 12:44:23 +0000

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import logo_broccoli from '@images/logo_broccoli.png'
+import logo_glasto from '@images/logo_glasto.png'
+import logo_great_escape from '@images/logo_great_escape.png'
+import logo_lyrix_organix from '@images/logo_lyrix_organix.png'
+import logo_sis from '@images/logo_sis.png'
+import logo_transmission from '@images/logo_transmission.png'
+import marnie_lean_1 from '@images/marnie_lean_1.jpeg'
+import marnie_lean_2 from '@images/marnie_lean_2.jpg'
+import marnie_lean_3 from '@images/marnie_lean_3.jpg'
+import marnie_listening from '@images/marnie_listening.jpg'
+import pod_backstage_roundhouse from '@images/pod_backstage_roundhouse.png'
+import pod_gentle_activism from '@images/pod_gentle_activism.png'
+import pod_glasto_commons from '@images/pod_glasto_commons.png'
+import pod_table_manners from '@images/pod_table_manners.png'
+import pod_tell_dem from '@images/pod_tell_dem.png'
+import pod_unreality from '@images/pod_unreality.png'
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main>
